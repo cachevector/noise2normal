cb4751eab2bb6104e206458a44fd940cb4685d99
maskedsyntax <aftaab2507@gmail.com>
2025-09-01 00:33:31 +0530

    feat: implement CLT to denoise image

--- main.cpp
+++ main.cpp
@@ -1,6 +1,99 @@
+#include "opencv2/core.hpp"
+#include "opencv2/core/hal/interface.h"
+#include "opencv2/core/mat.hpp"
+#include "opencv2/highgui.hpp"
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+// Add Gaussian Noise
+cv::Mat addGaussianNoise(cv::Mat image, double mean, double stddev) {
+    cv::Mat noise(image.size(), image.type());
+    cv::randn(noise, mean, stddev);
+    cv::Mat result = image + noise;
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    std::string image_path;
+    int n_samples = 5;
+    bool show_random = false;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if ((arg == "-i" || arg == "--image") && i + 1 < argc) {
+            image_path = argv[++i];
+        } else if (arg == "--sample-size" && i + 1 < argc) {
+            n_samples = std::stoi(argv[++i]);
+        } else if (arg == "--show-random-noisy-image") {
+            show_random = true;
+        }
+    }
+
+    if (image_path.empty()) {
+        std::cerr << "Usage: " << argv[0]
+                  << " -i <image_path> [--samples-size N] [--show-random-noisy-image]\n";
+
+        return 1;
+    }
+
+    std::cout << n_samples << std::endl;
+
+    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+    if (img.empty()) {
+        std::cerr << "Error: No image found.\n";
+        return -1;
+    }
+
+    // Convert image to grayscale
+    cv::Mat grayImage;
+    cv::cvtColor(img, grayImage, cv::COLOR_BGR2GRAY);
+
+    // Resize (400x300)
+    cv::Mat resized_img;
+    cv::Size new_size(800, 600);
+    resize(grayImage, resized_img, new_size, 0, 0, cv::INTER_LINEAR);
+
+    // generate noisy samples
+    std::vector<cv::Mat> noisy_images;
+    for (int i = 0; i < n_samples; ++i)
+        noisy_images.push_back(addGaussianNoise(resized_img, 0, 20));
+
+    /*
+     * Noise Reduction
+     *
+     * Uses Central Limit Theorem to reduce noise in the image
+     */
+    cv::Mat result(resized_img.size(), CV_32F, cv::Scalar(0));
+    for (int i = 0; i < n_samples; ++i) {
+        result += noisy_images[i];
+    }
+    result /= n_samples;
+    result.convertTo(result, CV_8U);
+
+    // --show-ramdom-noisy-image: true
+    // show random noisy sample
+    if (show_random) {
+        std::srand(std::time(0));
+        int idx = std::rand() % noisy_images.size();
+        cv::Mat random_sample;
+        noisy_images[idx].convertTo(random_sample, CV_8U);
+        cv::imshow("Random Noisy Sample", random_sample);
+    }
+
+    cv::imshow("Denoised Result", result);
+
+    int key;
+    for (;;) {
+        key = cv::waitKey(20);
+        if (key == 27 || key == 'q') {
+            break;
+        }
+    }
+
     return 0;
 }
